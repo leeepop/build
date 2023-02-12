a2bbc4fe6826ead229407ef09e4f9320e18379ba
leeepop <120262656+leeepop@users.noreply.github.com>
2023-02-12 05:55:27 +0000

    Update dns.py

--- dns.py
+++ dns.py
@@ -1,13 +1,19 @@
 import requests
 import json
 import os
-
+import socket
 napi = os.environ['NGROK_API']
 cf = os.environ['CF_API']
 zone = os.environ['ZONE']
 domain = os.environ['DOMAIN']
+domain2 = os.environ['DOMAIN2']
 service = os.environ['SERVICE']
-
+def get_ip_address(domain_name):
+    try:
+        return socket.gethostbyname(domain_name)
+    except socket.gaierror as e:
+        print("Error resolving hostname: %s" % e)
+        return None
 headers = {
         "Authorization": "Bearer "+napi,
         "Ngrok-Version": "2"
@@ -21,10 +27,29 @@ id = url[0]
 print(id)
 port = url[-5:]
 print(port)
+ip = get_ip_address(id+".tcp.ngrok.io")
 headers = {
             'Authorization': 'Bearer '+cf,
             'Content-Type': 'application/json'
         }
+try:
+        r = requests.get('https://api.cloudflare.com/client/v4/zones/+'zone'/dns_records?type=A&name='+domain2,headers=headers)
+        r = json.loads(r.text)
+        idd = r["result"][0]["id"]
+        print(idd)
+        r = requests.delete('https://api.cloudflare.com/client/v4/zones/+'zone'+/dns_records/'+idd,headers=headers)
+expect:
+        print('No A to delete')
+
+data = {
+    "type":"A",
+    "data":{
+        "name": domain2,
+        "content": ip,
+        "ttl":1,
+    }
+}
+a = requests.post('https://api.cloudflare.com/client/v4/zones/'+zone+'/dns_records',headers = headers,data = json.dumps(data))
 try:
         r = requests.get('https://api.cloudflare.com/client/v4/zones/+'zone'/dns_records?type=SRV',headers=headers)
         r = json.loads(r.text)
@@ -32,7 +57,7 @@ try:
         print(idd)
         r = requests.delete('https://api.cloudflare.com/client/v4/zones/+'zone'+/dns_records/'+idd,headers=headers)
 except:
-        print('Nothing to delete')
+        print('No SRV to delete')
 data = {
             "type":"SRV",
             "data":{
@@ -41,7 +66,7 @@ data = {
                 "priority":1,
                 "proto":"_tcp",
                 "service":service,
-                "target": id+".tcp.ngrok.io",
+                "target": domain2,
                 "weight":1,
                 "ttl":1,
             }
